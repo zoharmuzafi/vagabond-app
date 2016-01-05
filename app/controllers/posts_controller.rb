b3c81e072f8f413e4d2f9e15f34cdc039a86e5e3
alanlee35 <alanblee12@gmail.com>
2016-01-04 18:42:04 -0800

    post controller crud actions, and some small things in user profile page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,19 +1,66 @@
 class PostsController < ApplicationController
+  before_action :set_post, except: [:new, :create]
+  before_action :authorize, except: [:show]
+
   def new
+    if current_user
+      @post = Post.new
+    else
+      redirect_to user_path(current_user)
+      flash[:error] = 'You are already logged in'
+    end
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.user.id == current_user.id
+    if @post.save
+      redirect_to post_path(@post)
+      flash[:notice] = 'Post successfully created'
+    else
+      redirect_to new_post_path
+      flash[:error] = @post.errors.full_messages.join(', ')
+    end
   end
 
   def show
   end
 
   def edit
+    unless current_user.id == @post.user.id
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
+    if current_user == @post.user
+      if post.update_attributes(post_params)
+        redirect_to post_path(@post)
+        flash[:notice] = 'Post successfully updated'
+      else
+        redirect_to edit_post_path
+        flash[:error] = @post.errors.full_messages.join(', ')
+      end
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def destroy
+    if current_user == @post.user
+      @post.destroy
+      flash[:notice] = 'Post has been deleted'
+    end
+    redirect_to user_path(current_user)
+  end
+
+private
+  
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :city_id, :user_id)
   end
 end
