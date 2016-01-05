da9595aac66fadf296cd46af3fe747990c16dd72
stevennoble78 <stevennoble78@hotmail.com>
2016-01-04 18:31:02 -0800

    login and signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-   user = User.new(user_params)
-   if user.save
-     session[:user_id] = user.id
+   @user = User.new(user_params)
+   if @user.save
+     session[:user_id] = @user.id
      redirect_to user_path(current_user)
    else
      redirect_to signup_path
@@ -27,6 +28,6 @@ class UsersController < ApplicationController
 private
 
  def user_params
-   params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :city_id, :avatar)
  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
 
 	has_secure_password
 
+	validates :email, presence: true, uniqueness: true
+	validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
+  # validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
+  validates :password, length: { minimum: 8 }
+
 	belongs_to :city
 	has_many :posts, dependent: :destroy
 	has_many :comments, dependent: :destroy
