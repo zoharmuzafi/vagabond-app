c92505dc0a63aa9a5ed025cd9e14ef88e79ec321
stevennoble78 <stevennoble78@hotmail.com>
2016-01-04 18:49:54 -0800

    add flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,26 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
+    if current_user
+      redirect_to user_path(current_user)
+    else
+      @user = User.new
+    end
   end
 
   def create
-   @user = User.new(user_params)
-   if @user.save
-     session[:user_id] = @user.id
-     redirect_to user_path(current_user)
-   else
-     redirect_to signup_path
-   end
- end
+    if current_user
+      redirect_to user_path(current_user)
+    else
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        flash[:error] = @user.errors.full_messages.join(', ')
+        redirect_to signup_path
+      end
+    end
+  end
 
   def show
   end
@@ -20,14 +29,36 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user == @user
+      if @user.update_attributes(user_params)
+        redirect_to user_path(@user)
+      else
+        flash[:error] = @user.errs.full_messages.join(', ')
+        redirect_to edit_user_path(@user)
+      end
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def destroy
+    if current_user == @user
+      @user.destroy
+      session[:user_id] = nil
+      redirect_to login_path
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
-private
+  private
 
- def user_params
-   params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :city_id, :avatar)
- end
+    def user_params
+      params.require(:user).permit(:name, :username, :email, :password, :password_confirmation, :city_id, :avatar)
+    end
+
+    def set_user
+      user_id = params[:id]
+      @user = User.find_by_id(user_id)
+    end
 end
