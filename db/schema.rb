bbef5b3538adc17e8014aa0e61d7c645ca07937a
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-05 22:28:40 -0800

    updated master

--- db/schema.rb
+++ db/schema.rb
@@ -66,6 +66,8 @@ ActiveRecord::Schema.define(version: 20160106024317) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "username"
+    t.string   "uid"
+    t.string   "provider"
     t.string   "slug"
   end
 
