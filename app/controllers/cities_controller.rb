dce8d750ed0aac484ad354c78a5c8729ff93cfdd
YOUR_GITHUB_USERNAME <zohar.muzafi@gmail.com>
2016-01-06 20:00:29 -0800

    added no post div

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,4 +3,10 @@ class CitiesController < ApplicationController
   	@city = City.friendly.find(params[:id])
     @posts = Post.all.order("created_at DESC").page(params[:page]).per(4)
   end
+
+  def selectcity
+  	city = params.require(:city).permit(:city_id)["city_id"]
+  	find_city= City.find(city)
+  	redirect_to new_city_post_path(find_city)
+  end
 end
